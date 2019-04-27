1415424fc19553eeb448134c36f33dde5e826c3a
aditihaiman <ahaiman00@stuy.edu>
2019-04-27 09:36:10 -0400

    pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,13 @@
 public class MyHeap {
 
   private static void pushDown(int[]data,int size,int index){
-
+    int idx = index * 2 + 2;
+    while(idx > size){
+      if(data[idx-1] > data[idx]) idx--;
+      if(data[index] < data[idx]) swap(data, index, idx);
+      index = idx;
+      idx = idx * 2 + 2;
+    }
   }
 
   private static void pushUp(int[]data,int index) {
@@ -10,12 +16,20 @@ public class MyHeap {
   }
 
   public static void heapify(int[] data){
+    for(int x = 0; x < data.length; x++){
 
+    }
 
   }
 
   public static void heapsort(int[] data) {
-    
+
+  }
+
+  public static void swap(int[] data, int index, int idx){
+    int temp = data[idx];
+    data[idx] = data[index];
+    data[index] = temp;
   }
 
 
